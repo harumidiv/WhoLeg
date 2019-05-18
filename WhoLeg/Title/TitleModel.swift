674a758c48c4e66faae3256353212e96cfd1f510
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-05-19 00:37:58 +0900

    feat:Create title model

---
+++ WhoLeg/Title/TitleModel.swift
@@ -0,0 +1,28 @@
+//
+//  TitleModel.swift
+//  WhoLeg
+//
+//  Created by 佐川晴海 on 2019/05/18.
+//  Copyright © 2019 佐川晴海. All rights reserved.
+//
+
+import Foundation
+
+protocol TitleModelDelegate {
+    
+}
+
+class TitleModel {
+    func getQuizJson() -> QuizInfo {
+        let path: String = Bundle.main.path(forResource: "Resource/quiz", ofType: "json")!
+        var jsonData: QuizInfo!
+        do {
+            let content = try String(contentsOfFile: path)
+            jsonData = try JSONDecoder().decode(QuizInfo.self, from: content.data(using: .utf8)!)
+            return jsonData
+        } catch  {
+            // TODO: Error handling
+            return jsonData
+        }
+    }
+}
