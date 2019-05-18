4cffbc420c8621e52d4681325b313b2f73c7153b
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-05-18 12:12:44 +0900

    Create a project

---
+++ WhoLeg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhoLeg
+//
+//  Created by 佐川晴海 on 2019/05/18.
+//  Copyright © 2019 佐川晴海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
