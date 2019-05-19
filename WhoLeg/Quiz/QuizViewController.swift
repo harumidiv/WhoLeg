03b3074115110a512ca3ce4b1030cd7170603d9f
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-05-19 17:50:01 +0900

    fix: Fixed app display name

--- WhoLeg/Quiz/QuizViewController.swift
+++ WhoLeg/Quiz/QuizViewController.swift
@@ -2,22 +2,29 @@
 //  QuizViewController.swift
 //  WhoLeg
 //
-//  Created by 佐川晴海 on 2019/05/18.
+//  Created by 佐川晴海 on 2019/05/19.
 //  Copyright © 2019 佐川晴海. All rights reserved.
 //
 
 import UIKit
 
 class QuizViewController: UIViewController {
-    
-    private(set) lazy var myView: QuizView = QuizView()
-
-    override func loadView() {
-        view = myView
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
     }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
