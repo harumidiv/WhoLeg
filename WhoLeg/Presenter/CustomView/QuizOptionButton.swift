66e7c6bd86390a220abfff5f1f637e0fccccf820
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-05-19 18:03:52 +0900

    feat: Add custom button

---
+++ WhoLeg/Presenter/CustomView/QuizOptionButton.swift
@@ -0,0 +1,35 @@
+//
+//  QuizOptionButton.swift
+//  WhoLeg
+//
+//  Created by 佐川晴海 on 2019/05/19.
+//  Copyright © 2019 佐川晴海. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class QuizOptionButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupAttribute()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupAttribute()
+    }
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupAttribute()
+    }
+    private func setupAttribute(){
+        layer.cornerRadius = self.frame.height/2
+        layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
+        backgroundColor = UIColor(hex: "#F4B400")
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 40.0)
+    }
+
+}
