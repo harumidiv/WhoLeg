472cf698bbb12f9b1f6a07b456e7ddd0c99e1bc3
harumidiv <harumidiv@gmail.com>
2020-01-27 18:39:08 +0900

    feat: RunScriptを追加しました

---
+++ Script/ValidateJson/ValidateJson.swift
@@ -0,0 +1,10 @@
+//
+//  VaridateJson.swift
+//  WhoLeg
+//
+//  Created by 佐川 晴海 on 2020/01/25.
+//  Copyright © 2020 佐川晴海. All rights reserved.
+//
+
+print("hello shellScript")
+// fatalError("Local Json Parse Error")
