afe303cb24370a7260f9a5958072d735fda3ca63
harumidiv <harumidiv@gmail.com>
2020-01-28 21:21:05 +0900

    feat: jsonの答えが存在するかのテストをRunScriptに追加しました

--- Script/ValidateJson/ValidateJson.swift
+++ Script/ValidateJson/ValidateJson.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2020 佐川晴海. All rights reserved.
 //
 
-print("hello shellScript")
-
 let fileManager = FileManager.default
 let path = fileManager.currentDirectoryPath
 print(path)
@@ -17,7 +15,15 @@ let content = try! String(contentsOfFile: "\(path)/WhoLeg/Resource/quiz.json")
 let decoder = JSONDecoder()
 decoder.keyDecodingStrategy = .convertFromSnakeCase
 do {
-    _ = try decoder.decode(QuizInfo.self, from: content.data(using: .utf8)!)
+    let quizData = try decoder.decode(QuizInfo.self, from: content.data(using: .utf8)!)
+
+    // クイズの中に答えと選択肢で一致しないものがないかのテスト
+    quizData.quiz.forEach { q in
+        if q.answer == q.choice.a || q.answer == q.choice.b || q.answer == q.choice.c
+            || q.answer == q.choice.d {} else {
+            fatalError("【\(q.answer)】の答えが選択肢にありません！")
+        }
+    }
 } catch {
-    fatalError("Local Json Parse Error")
+    fatalError("Jsonの構造が壊れています")
 }
