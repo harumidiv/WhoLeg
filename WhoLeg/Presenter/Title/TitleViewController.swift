25ce581e3f4a2d7362c8c4c3e940fce097cbd360
Harumi Sagawa <h_sagawa@yumemi.co.jp>
2026-05-26 09:31:02 +0900

    タイトル画面をSwiftUIに置き換え

--- WhoLeg/Presenter/Title/TitleViewController.swift
+++ WhoLeg/Presenter/Title/TitleViewController.swift
@@ -7,53 +7,61 @@
 //
 
 import UIKit
+import SwiftUI
 
-class TitleViewController: UIViewController {
-    @IBOutlet weak var roundBaseView: RoundView!
-    @IBOutlet weak var image: RoundImage!
-    @IBOutlet weak var label: UILabel!
+class TitleViewController: UIHostingController<TitleScreen> {
+
     private var presenter: TitlePresenter!
+    private var jsonData: QuizInfo!
 
-    var jsonData: QuizInfo!
+    init() {
+        super.init(rootView: TitleScreen(imageName: "", answerText: "", onStart: {}))
+    }
+
+    @MainActor required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     func injector(presenter: TitlePresenter) {
         self.presenter = presenter
     }
 
-    // MARK: - LifeCycle
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.largeTitleDisplayMode = .never
+
+        jsonData = presenter.getJsonData()
+        let rand = Int.random(in: 0 ..< jsonData.quiz.count)
+        let quiz = jsonData.quiz[rand]
+        let imageName = URL(fileURLWithPath: quiz.image).deletingPathExtension().lastPathComponent
+
+        rootView = TitleScreen(
+            imageName: imageName,
+            answerText: quiz.answer,
+            onStart: { [weak self] in self?.quizStart() }
+        )
+
         title = "だれのあし？"
         navigationItem.hidesBackButton = true
-        jsonData = presenter.getJsonData()
-
-        let rand = Int.random(in: 0 ..< jsonData.quiz.count)
-        image.image = UIImage(named: jsonData.quiz[rand].image)
-        label.text = jsonData.quiz[rand].answer
-
-        let informationButton: UIBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "information"), style: .plain, target: self, action: #selector(showInformation(_:)))
-        navigationItem.rightBarButtonItem = informationButton
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "information"),
+            style: .plain,
+            target: self,
+            action: #selector(showInformation)
+        )
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        image.layer.cornerRadius = image.frame.width / 2
-        roundBaseView.layer.cornerRadius = roundBaseView.frame.width / 2
-    }
-
-    // MAKR: - Event
-
-    @objc func showInformation(_ sender: UIBarButtonItem) {
+    @objc private func showInformation() {
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         navigationController?.pushViewController(InformationViewController(data: jsonData), animated: true)
     }
 
-    @IBAction func quizStart(_ sender: Any) {
+    private func quizStart() {
         let quizViewController = QuizViewController(quizData: jsonData, score: 0, count: 1)
-        quizViewController.injector(presenter: QuizPresenterImpl(model: QuizModelImpl(), output: quizViewController), wireframe: QuizWireframeImpl())
+        quizViewController.injector(
+            presenter: QuizPresenterImpl(model: QuizModelImpl(), output: quizViewController),
+            wireframe: QuizWireframeImpl()
+        )
         navigationController?.pushViewController(quizViewController, animated: true)
     }
 }
